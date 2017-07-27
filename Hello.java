2b19e58741439587df311620b3273398a0f86f00
Amol Office <info.amol@gmail.com>
2017-07-27 09:12:22 -0400

    office:6:

--- Hello.java
+++ Hello.java
@@ -1,11 +1,13 @@
 public class Hello {
 
 public static void main(String[] args) {
+System.out.println("\n\n Loop started.......");
 
 for(int i=0;i<100;i++) {
 System.out.println(" From office :Hello Universe...."+i);
 
 }
 System.out.println("\n\n End of The Loop.......");
+
 }
 }
