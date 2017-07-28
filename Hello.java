eb2a697c995c1d0d0fb6c29d023f81db52e65344
Amol Wankhede <info.amol@gmail.com>
2017-07-28 06:19:08 +0530

    Please include useful commit message!

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 public static void main(String[] args) {
 System.out.println("\n\n Loop started.......");
 
-for(int i=0;i<10;i++) {
+for(int i=11111111111;i<10;i++) {
 System.out.println(" From office :Hello Universe...."+i);
 
 }
